e3f77382f4e3a21ee80debc637776b6ab63aadde
Iqbal Syamil Ayasy <iqbalsyamilayas@gmail.com>
2019-11-01 13:39:00 +0700

    update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
-    "extends": "standard"
-};
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+  }
+}
