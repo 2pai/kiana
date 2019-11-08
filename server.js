225f39059a2f6a046f6c23fcf02f7b91af8fa1da
Iqbal Syamil Ayasy <iqbalsyamilayas@gmail.com>
2019-11-08 23:07:38 +0700

    add influxdb

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const App = require('./src/app')
 const mqtt = require('./src/lib/connection/mqtt')
+const influxDB = require('./src/lib/db/influxdb')
 require('dotenv').config()
 
 const server = new App()
 
 server.init(process.env.PORT || 9000, () => {
-/* TODO :
-    - init influxDB Connection
+  influxDB.init() // bug
+  /* TODO :
     - init redis Connection
-*/
+  */
   mqtt.init()
 })
