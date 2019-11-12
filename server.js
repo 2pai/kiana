57a77c5b3ad261b37f296fdc922b3f7016e89f7d
Iqbal Syamil Ayasy <iqbalsyamilayas@gmail.com>
2019-11-13 01:55:18 +0700

    add init for mongodb,redis,influxdb

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const App = require('./src/app')
 const mqtt = require('./src/lib/connection/mqtt')
 const influxDB = require('./src/lib/db/influxdb')
+const mongodb = require('./src/lib/db/mongodb')
+const redis = require('./src/lib/cache/redis')
 require('dotenv').config()
 
 const server = new App()
 
 server.init(process.env.PORT || 9000, () => {
-  influxDB.init() // bug
-  /* TODO :
-    - init redis Connection
-  */
+  influxDB.init()
+  mongodb.init()
+  redis.init()
   mqtt.init()
 })
