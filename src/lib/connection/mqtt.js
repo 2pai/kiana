10e496de14cd8604768df8116c208182cead88a9
Iqbal Syamil Ayasy <iqbalsyamilayas@gmail.com>
2019-11-17 02:25:56 +0700

    init subscirbe to list topic

--- src/lib/connection/mqtt.js
+++ src/lib/connection/mqtt.js
@@ -3,24 +3,40 @@ const mqtt = require('mqtt')
 const logger = require('../helper/logger')
 const _ = require('lodash')
 const client = mqtt.connect(process.env.MQTT_URL)
-
+const { getListDevice } = require('../../repository/queries/queriesModel')
 const init = () => {
   client.on('connect', () => {
-    /* fetch list topic -> subscribe to topic */
     subscribe('sensor-data')
+    getListDevice.find({}, { topicDevice: 1, _id: 0 }, async (err, doc) => {
+      if (err) logger.error('mqtt-init', JSON.stringify(err))
+      doc.map(async (val) => {
+        logger.info('mqtt-init', 'Subscribe to : ' + val.topicDevice)
+        await subscribe(val.topicDevice)
+      })
+    })
+
     logger.info('mqtt-init', 'MQTT connected')
   })
 }
 
-const publish = (topic, msg) => {
+const publish = async (topic, msg) => {
   client.publish(topic, msg.toString(), { qos: 2 }, (err) => {
     if (err) logger.error('mqtt-publish', JSON.stringify(err))
+    logger.info('mqtt-publish', 'message published to ' + topic)
   })
 }
 
-const subscribe = (topic) => {
+const subscribe = async (topic) => {
   client.subscribe(topic, { qos: 2 }, (err) => {
     if (err) logger.error('mqtt-subscribe', JSON.stringify(err))
+    logger.info('mqtt-subscribe', 'success subscribe to ' + topic)
+  })
+}
+
+const unsubscribe = async (topic) => {
+  client.unsubscribe(topic, { qos: 2 }, (err) => {
+    if (err) logger.error('mqtt-unsubscribe', JSON.stringify(err))
+    logger.info('mqtt-unsubscribe', 'success unsubscribe to ' + topic)
   })
 }
 
@@ -34,5 +50,6 @@ client.on('message', (topic, message) => {
 module.exports = {
   init,
   publish,
-  subscribe
+  subscribe,
+  unsubscribe
 }
